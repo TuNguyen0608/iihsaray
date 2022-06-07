6d48c3fda00018f2c2a67c0b26cabad34e33f930
TuNguyen0608 <103015212+TuNguyen0608@users.noreply.github.com>
2022-06-07 00:35:53 -0700

    added cats

--- js/blank.js
+++ js/blank.js
@@ -1,20 +1,30 @@
-var t2 = new SplitText('.txt2').chars,
-    color2 = '#17c0fd',
-    color1 = '#fff',
-    moveBar = ()=>{ gsap.set('.bar', {left:gsap.getProperty('.txt1','width') + 1}) };
+let slideIndex = 1;
+showSlides(slideIndex);
 
-gsap.timeline({delay:0.2})
-    .set('.txt1',   {color:color1, fontWeight:'regular'})
-    .set('.txt2',   {color:color2, fontWeight:'bold', opacity:0, x:gsap.getProperty('.txt1','width')-2, immediateRender:true})
-    .set('.bar',    {left:1, backgroundColor:color1, immediateRender:true})
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
 
-    .to('.bar',     {duration:0.1, opacity:0, ease:Expo.easeIn, yoyo:true, repeat:5, repeatDelay:0.3}, 0)
-    .from('.txt1',  {duration:1.1, width:0, ease:SteppedEase.config(14), onUpdate:moveBar}, 2.5)
-    .to('.bar',     {duration:0.05, backgroundColor:color2}, '+=0.15')
-    .to('.bar',     {duration:1.0, width:290, ease:Power4.easeInOut}, '+=0.1')
-    .from('.container',     {duration:1.0, x:135, ease:Power4.easeInOut}, '-=1.0')
-    .to('.txt2',    {duration:0.01, opacity:1}, '-=0.1')
-    .to('.bar',     {duration:0.4, x:290, width:0, ease:Power4.easeIn})
-    .from(t2,       {duration:0.6, opacity:0, ease:Power3.easeInOut, stagger:0.02}, '-=0.5')
-    .to('.txt1',    {duration:1.5, opacity:0.25, ease:Power3.easeInOut}, '-=1.2')
-    .timeScale(1.45)
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
